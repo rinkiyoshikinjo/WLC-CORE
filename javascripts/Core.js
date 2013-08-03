97ef08b9d15a85f079ea6d6319f63b4de0840151
rinkiyoshikinjo <rinkiyoshikinjo@gmail.com>
2013-08-03 06:23:36 +0100

    Update Core.js

--- javascripts/Core.js
+++ javascripts/Core.js
@@ -1,4 +1,4 @@
-//----------------------Do not edit this script unless you know what you are doing----------------------nexter2
+//----------------------Do not edit this script unless you know what you are doing----------------------nexter3
 
 //Variables for the script
 var c = user;
